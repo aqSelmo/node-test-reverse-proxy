2243894e655a6780e1cb3d5dd76a0b88a3a38866
Guilherme Falcao <guilusa25@gmail.com>
2021-03-31 02:34:08 -0300

    version 4

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.get("/", (request, response) => response.send('Teste'));
 
-app.listen(3000, '0.0.0.0', () => console.log('funcionando'));
+app.listen(80, '0.0.0.0', () => console.log('funcionando'));
